f460442b34c8dfbf808fc3f641ae1a74149ec430
adonay-ar <ar20012@ues.edu.sv>
2023-08-23 20:35:44 -0600

    First commit

---
+++ resources/views/content/clientes/cliente.blade.php
@@ -0,0 +1,13 @@
+@php
+  $container = 'container-fluid';
+  $containerNav = 'container-fluid';
+@endphp
+
+@extends('layouts/contentNavbarLayout')
+@section('title', 'Clientes')
+@section('content')
+  <div class="d-flex align-items-center justify-content-between py-3">
+    <h4 class="fw-bold py-1 m-0"><span class="text-muted fw-light">Clientes /</span> Cartera de Clientes</h4>
+    <a class="btn btn-primary" type="button" href="{{url('pages/account-settings-connections')}}"><span class="tf-icons bx bx-plus-circle"></span> Nuevo Cliente</a>
+  </div>
+@endsection
